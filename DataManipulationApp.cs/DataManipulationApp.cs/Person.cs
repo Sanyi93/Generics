f91348f768f29210bbadb0814080cb47da7f1286
Sanyi93 <105607253+Sanyi93@users.noreply.github.com>
2025-07-15 16:24:08 +0200

    Add files via upload
    DataManipulation - LINQ added

---
+++ DataManipulationApp.cs/DataManipulationApp.cs/Person.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManipulationApp.cs
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String City { get; set; }
+
+        //overriding the toString method to easily print the Person objects
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, City: {City}";
+        }
+
+
+        // A separate class to manage the collection of persons and perform queries
+        public static class PersonRepository
+        {
+
+            //a static readonly list
+            private static readonly List<Person> allPersons = new List<Person>
+            {
+                new Person {Id = 1, Name ="Martin", City = "Wien"},
+                new Person {Id = 2, Name = "Marian", City = "Innsbruck"},
+                new Person {Id = 3, Name = "Florian", City = "Wiener Neustadt"},
+                new Person {Id = 4, Name = "Tim", City = "Dornbirn"},
+                new Person {Id = 5, Name = "Oliver", City = "Klagenfurt"},
+                new Person {Id = 6, Name = "Karl", City = "Graz"},
+                new Person {Id = 7, Name = "Oliver", City = "Wien"},
+                new Person {Id = 8, Name = "Marian", City = "Graz"},
+                new Person {Id = 9, Name = "Ema", City = "Innsbruck"},
+                new Person {Id = 10, Name = "Petra", City = "Klagenfurt"}
+            };
+
+
+            // Methods now return IEnumerable<Person> for flexibility, not arrays or void
+            public static IEnumerable<Person> SelectById(int id)
+        {
+
+            return allPersons.Where(p => p.Id == id);
+
+            //Person[] personById = allPersons.Where(p => p.Id == Id).ToArray();
+            //foreach (Person person in personById) 
+            //{
+            //    Console.WriteLine(person.Name);
+               
+            //}
+        }
+
+        public static IEnumerable<Person> SelectByName(string name)
+        {
+
+                return allPersons.Where(p => p.Name == name);
+
+                //Person[] personByName = allPersons.Where(p => p.Name == Name).ToArray();
+                //foreach(Person person in personByName)
+                //{
+                //    Console.WriteLine(person.Name);
+                //}
+            }
+
+            public static IEnumerable<Person> SelectByCity(string city)
+        {
+
+                return allPersons.Where(p => p.City == city);
+
+            //Person[] personByCity = allPersons.Where(p => p.City == City).ToArray();
+            //foreach(Person person in personByCity)
+            //{
+            //    Console.WriteLine(person.Name);
+            //}
+        }
+       
+        }
+    }
+    
+}
---
+++ DataManipulationApp.cs/DataManipulationApp.cs/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataManipulationApp.cs;
+
+while (true)
+{
+    Console.WriteLine("Welcome to the DataManipulationApp.");
+    Console.WriteLine("Please select the criterion ('id, 'name', 'city' or 'exit' to close the app) based on which you wish to select the person");
+    string criterion = Console.ReadLine().ToLower();
+
+    if (criterion == "exit")
+    {
+        Console.WriteLine("Thanks for using the app. Have a nice day!");
+        break;
+    }
+
+    switch (criterion)
+    {
+        case "id":
+            Console.WriteLine("Thanks for selecting the id criterion. Please enter the id: ");
+            Console.ReadLine();
+            if(!int.TryParse(Console.ReadLine(), out int id))
+            {
+                Console.WriteLine("Invalid input, try again please");
+
+            }
+            IEnumerable<Person> personsById = Person.PersonRepository.SelectById(id);
+            foreach(Person person in personsById)
+            {
+                Console.WriteLine($"The people with id {id} : " + person.ToString());
+            }
+            break;
+        case "name":
+            Console.WriteLine("Thanks for selecting the name criterion. Please enter the name: ");
+            string name = Console.ReadLine();
+            IEnumerable<Person> personsByName = Person.PersonRepository.SelectByName(name);
+            foreach(Person person in personsByName)
+            {
+                Console.WriteLine($"The people with name {name}: " + person.ToString());
+            }
+            break;
+        case "city":
+            Console.WriteLine("Thanks for selecting the city criterion. Please enter the city: ");
+            string city = Console.ReadLine();
+            IEnumerable<Person> personsByCity = Person.PersonRepository.SelectByCity(city);
+            foreach(Person person in personsByCity)
+            {
+                Console.WriteLine($"The people with city {city}: " + person.ToString());
+            }
+            break;
+        default: Console.WriteLine("Incorrect input. Try again, please.");
+            break;
+    }
+   
+}
+
+
+
+
+
